e2bbbc4531421bcce7343c884bc23a573523fe7a
sivabharathy <gotocva@gmail.com>
2019-10-09 14:46:58 +0530

    connection close handled

--- server.js
+++ server.js
@@ -5,13 +5,35 @@ const wss = new WebSocketServer({port: 8080});
 let CLIENTS=[];
 
 wss.on('connection', function(ws) {
-    CLIENTS.push(ws);
+    // CLIENTS.push(ws);
     ws.on('message', function(message) {
-        console.log('received: %s', message);
-        sendAll(message);
+        message = JSON.parse(message);
+
+        if (message.action === 'INIT') {
+            CLIENTS[message.id] = ws;
+            listenAll();
+        }
+
+        if (message.action === 'SHOW') {
+            CLIENTS.forEach( function(socket,i){
+                //socket.on('close', console.log("Connection closed"+i));
+                console.log(socket);
+                console.log(i);
+            });
+        }
+        // sendAll(message);
     });
     ws.send("NEW USER JOINED");
 });
+
+
+function listenAll(){
+    CLIENTS.forEach( function(socket,i){
+        socket.on('close', function(){
+            console.log("Connection closed"+i)
+        });
+    });
+}
 
 function sendAll (message) {
     for (var i=0; i<CLIENTS.length; i++) {
